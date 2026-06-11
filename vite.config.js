6a4e82549a126bdae210cbc0326373b693e4bbca
lingsanbuer <lingsanbuer@users.noreply.github.com>
2026-06-12 02:24:56 +0800

    build: output to docs/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: { port: 3000, open: true },
-  base: './'
+  base: './',
+  build: { outDir: 'docs' }
 })
